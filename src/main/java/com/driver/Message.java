0d06ee0436e9634edf79a7036c59cf830366638e
Reshwanth <rachutireshwanth@gmail.com>
2023-01-29 15:29:43 +0530

    done

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,39 +1,107 @@
+//package com.driver;
+//
+//import java.util.Date;
+//
+//public class Message {
+//    private int id;
+//    private String content;
+//    private Date timestamp;
+//    public Message(){}
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public void setTimestamp(Date timestamp) {
+//        this.timestamp = timestamp;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public Date getTimestamp() {
+//        return timestamp;
+//    }
+//
+//    public Message(int id,String content){
+//        setContent(content);
+//        setTimestamp(timestamp);
+//    }
+//}
 package com.driver;
 
+
+
+
+import javax.annotation.processing.Generated;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 
 public class Message {
+
     private int id;
     private String content;
     private Date timestamp;
-    public Message(){}
 
-    public void setId(int id) {
-        this.id = id;
+    private Message message;
+    private Group group;
+
+    public Message getMessage() {
+        return message;
     }
 
-    public void setContent(String content) {
+    public Message(int id, String content) {
+        this.id = id;
         this.content = content;
     }
 
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getContent() {
         return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public Date getTimestamp() {
         return timestamp;
     }
 
-    public Message(int id,String content){
-        setContent(content);
-        setTimestamp(timestamp);
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Message() {
     }
 }
--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -1,6 +1,67 @@
+//package com.driver;
+//public class User {
+//    private String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    private String mobile;
+//
+//    public String getMobile() {
+//        return mobile;
+//    }
+//
+//    public void setMobile(String mobile) {
+//        this.mobile = mobile;
+//    }
+//
+//    public User (String name, String mobile){
+//        setMobile(mobile);
+//        setName(name);
+//    }
+//    public User(){}
+//}
 package com.driver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String name;
+    private String mobile;
+
+    private List<Message> messageList = new ArrayList<>();
+    private Group group;
+
+    public List<Message> getMessageList() {
+        return messageList;
+    }
+
+
+    public void setMessageList(List<Message> messageList) {
+        this.messageList = messageList;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public User() {
+    }
 
     public String getName() {
         return name;
@@ -10,8 +71,6 @@ public class User {
         this.name = name;
     }
 
-    private String mobile;
-
     public String getMobile() {
         return mobile;
     }
@@ -19,10 +78,4 @@ public class User {
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
-
-    public User (String name, String mobile){
-        setMobile(mobile);
-        setName(name);
-    }
-    public User(){}
 }
